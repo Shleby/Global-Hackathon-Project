43eca7281166d4984ea1632397788cff5bc97e85
NEUDitao <li.edwa@husky.neu.edu>
2020-03-27 19:35:32 -0400

    styler

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -1,28 +1,28 @@
-import React from "react";
-import "./Navbar.scss";
-import Logo from "../Images/logo.png";
-import { Flex, FlexItem } from "@patternfly/react-core";
-import SettingsIcon from "@material-ui/icons/Settings";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import React from 'react';
+import './Navbar.scss';
+import { Flex, FlexItem } from '@patternfly/react-core';
+import SettingsIcon from '@material-ui/icons/Settings';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Logo from '../Images/logo.png';
 
 export const Navbar: React.FC = () => {
   return (
-    <Flex className="navbar-container">
+    <Flex className='navbar-container'>
       <FlexItem>
-        <img src={Logo} alt="Ping-Logo" className="nav-logo" />
+        <img src={ Logo } alt='Ping-Logo' className='nav-logo' />
       </FlexItem>
       <FlexItem>
-        <a href="/" className="nav-title">
+        <a href='/' className='nav-title'>
           Ping
         </a>
       </FlexItem>
       <FlexItem>
-        <div style={{ float: "right" }}>
+        <div style={{ float: 'right' }}>
           <SettingsIcon />
         </div>
       </FlexItem>
       <FlexItem>
-        <div style={{ float: "right" }}>
+        <div style={{ float: 'right' }}>
           <AccountCircleIcon />
         </div>
       </FlexItem>
