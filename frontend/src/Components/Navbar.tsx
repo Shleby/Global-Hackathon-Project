4a5652d1e5a4c5d12955d92e5e45e012cb3002b1
Shleby <shelby.a.huffy@gmail.com>
2020-03-28 12:15:29 -0500

    for cheese

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -1,74 +1,30 @@
-import React from 'react';
-import './Navbar.scss';
-import SettingsIcon from '@material-ui/icons/Settings';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import Logo from '../Images/logo.png';
-import {SearchBar} from "./SearchBar"
-import styled from 'styled-components'
+import React from "react";
+import "./Navbar.scss";
+import SearchIcon from "@material-ui/icons/Search";
 
+import Logo from "../Images/logo.png";
+import { AppBar, Toolbar, Button } from "@material-ui/core";
 
 interface PageProps {
-  mainPage: boolean,
+  mainPage: boolean;
 }
 
-const StyledNavbar = styled.div`
-  height: 5vh;
-  width: 100%;
-  background-image: linear-gradient(to bottom, #ffffff 90%, #DDDDDD 100%);
-  margin-top: 2%;
-
-  display: flex;
-  .logo-container{
-    //border: 1px green solid;
-    display: flex;
-    width: 100%;
-    img{
-
-    }
-    .nav-title{
-      
-    }
-  }
-  .search-bar{
-    width: 100%;
-  }
-  .nav-icons-container{
-    width: 90%;
-    margins: auto;
-    display: flex;
-    justify-content: flex-end;
-    .nav-icons{
-      height: 100%;
-    }
-  }
-`
-
-function checkPage(mainPage: boolean){
-  if(!mainPage)
-  return <SearchBar />
-}
-
-export const Navbar: React.FC<PageProps> = (mainPage) => {
+export const Navbar: React.FC<PageProps> = mainPage => {
   return (
-
-    <StyledNavbar>
-      <div className="logo-container">
-        <img src={ Logo } alt='Ping-Logo' className='nav-logo' />
-        
-        <a href='/' className='nav-title'>
-            Ping
-        </a>
-      </div>
-      <div className = "search-bar">
-      <SearchBar />
-   
-      </div>
-      <div className="nav-icons-container">
-        <div className='nav-icons'>
-            <SettingsIcon />
-            <AccountCircleIcon />
+    <AppBar position="static" color="inherit" className="navbar-container">
+      <Toolbar>
+        <img src={Logo} alt="logo" className="nav-logo" />
+        <h2 className="nav-title">Ping</h2>
+        <div>
+          <SearchIcon />
         </div>
-      </div>
-    </StyledNavbar>
+        <Button color="inherit" className="nav-link">
+          Settings
+        </Button>
+        <Button color="inherit" className="nav-link">
+          Login
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 };
--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,45 +1,42 @@
 import React from "react";
-import styled from 'styled-components';
-import { SearchBar } from "../Components/SearchBar"
-import {Navbar} from "../Components/Navbar"
+import styled from "styled-components";
+
+import { Navbar } from "../Components/Navbar";
 
 const StyledHome = styled.div`
-  text-align:center;
+  text-align: center;
 
-  .container{
+  .container {
     margin: auto;
     position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    
+
     h2 {
       color: ${props => props.theme.main};
       font-size: 64px;
-      background:transparent;
+      background: transparent;
       margin-bottom: 10px;
     }
-    .subtitle{
+    .subtitle {
       color: ${props => props.theme.black};
       font-size: 20px;
       margin-bottom: 50px;
     }
   }
-`
+`;
 
 export const Home: React.FC = () => {
   return (
     <div>
-      <Navbar mainPage={true}/>
+      <Navbar mainPage={true} />
       <StyledHome>
         <div className="container">
-          <h2>
-            Inventory Ping
-          </h2>
+          <h2>Inventory Ping</h2>
           <div className="subtitle">
             Search for much needed items in your local items in your area.
           </div>
-          <SearchBar />
         </div>
       </StyledHome>
     </div>
