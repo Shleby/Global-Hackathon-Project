a1d017df419b6d27aec72a408f85088ce931d20a
Shleby <shelby.a.huffy@gmail.com>
2020-03-27 22:06:52 -0500

    last commit of the day

--- frontend/src/Pages/Search.tsx
+++ frontend/src/Pages/Search.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
-import { Navbar } from '../Components/Navbar';
+import React, { useState, useEffect } from "react";
+import { Navbar } from "../Components/Navbar";
+import { AgGridReact } from "ag-grid-react";
 
 export const Search: React.FC = () => {
-  return <Navbar mainPage={false}/>;
+  const [columnDefs, setColumnDefs] = useState();
+  const [rowData, setRowData] = useState();
+
+  useEffect(() => {
+    fetch("./jsonFolder/searchFakeBackend.json")
+      .then(req => req.json())
+      .then(res => {
+        setColumnDefs(res.columnDefs);
+        setRowData(res.rowData);
+      });
+  });
+  return (
+    <div>
+      <Navbar mainPage={false} />
+      <AgGridReact columnDefs={columnDefs} rowData={rowData}></AgGridReact>
+    </div>
+  );
 };
