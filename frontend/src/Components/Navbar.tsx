66730b43640adaea0087aace877bccd70cb411d5
Seth Gaglione <sethgaglione@users.noreply.github.com>
2020-03-27 22:09:25 -0500

    Up to date navbar

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -1,62 +1,74 @@
-import React from "react";
-import "./Navbar.scss";
+import React from 'react';
+import './Navbar.scss';
+import SettingsIcon from '@material-ui/icons/Settings';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Logo from '../Images/logo.png';
+import {SearchBar} from "./SearchBar"
+import styled from 'styled-components'
 
-import Logo from "../Images/logo.png";
-import styled from "styled-components";
 
 interface PageProps {
-  mainPage: boolean;
+  mainPage: boolean,
 }
 
 const StyledNavbar = styled.div`
-  height: 6vh;
+  height: 5vh;
   width: 100%;
-  border-bottom-style: solid;
-  border-bottom-width: thin;
+  background-image: linear-gradient(to bottom, #ffffff 90%, #DDDDDD 100%);
+  margin-top: 2%;
+
   display: flex;
-  background-color: rgb(194, 194, 194);
+  .logo-container{
+    //border: 1px green solid;
+    display: flex;
+    width: 100%;
+    img{
 
-  img {
+    }
+    .nav-title{
+      
+    }
   }
-  .nav-title {
-    color: #3d3f70;
-    margin: 0;
-    text-decoration: none;
-    font-size: 5vh;
+  .search-bar{
+    width: 100%;
   }
-`;
+  .nav-icons-container{
+    width: 90%;
+    margins: auto;
+    display: flex;
+    justify-content: flex-end;
+    .nav-icons{
+      height: 100%;
+    }
+  }
+`
 
-export const Navbar: React.FC<PageProps> = mainPage => {
+function checkPage(mainPage: boolean){
+  if(!mainPage)
+  return <SearchBar />
+}
+
+export const Navbar: React.FC<PageProps> = (mainPage) => {
   return (
+
     <StyledNavbar>
-      <img src={Logo} alt="Ping-Logo" className="nav-logo" />
-
-      <a href="/" className="nav-title">
-        Ping
-      </a>
-    </StyledNavbar>
-    /*
-    <Flex className='navbar-container'>
-      <FlexItem>
+      <div className="logo-container">
         <img src={ Logo } alt='Ping-Logo' className='nav-logo' />
-      </FlexItem>
-      <FlexItem>
+        
         <a href='/' className='nav-title'>
-          Ping
+            Ping
         </a>
-      </FlexItem>
-      <FlexItem />
-      <FlexItem breakpointMods={ [{ modifier: FlexModifiers['align-right'] }] }>
+      </div>
+      <div className = "search-bar">
+      <SearchBar />
+   
+      </div>
+      <div className="nav-icons-container">
         <div className='nav-icons'>
-          <SettingsIcon />
+            <SettingsIcon />
+            <AccountCircleIcon />
         </div>
-      </FlexItem>
-
-      <FlexItem>
-        <div className='nav-icons'>
-          <AccountCircleIcon />
-        </div>
-      </FlexItem>
-    </Flex>*/
+      </div>
+    </StyledNavbar>
   );
 };
